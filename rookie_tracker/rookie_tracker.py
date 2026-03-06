c04f9a2e763f7a2f5450221e44e4029383b92830
timesnewronann <ronanborja26@gmail.com>
2026-03-05 18:05:51 -0800

    Working on adjusting ROI so ball is properly tracked

--- rookie_tracker/rookie_tracker.py
+++ rookie_tracker/rookie_tracker.py
@@ -9,7 +9,7 @@ LOW_ORANGE = (5, 100, 100)
 HIGH_ORANGE = (25, 255, 255)
 
 # Restrict the search for the ball to the hoop area/ avoid random oranges
-ROI = (1536, 250, 3840, 1620)
+ROI = (1536, 450, 3840, 1620)
 
 
 def detect_ball_center_stub(frame, roi=None):
@@ -35,6 +35,7 @@ def detect_ball_center_stub(frame, roi=None):
     # crop to ROI
     crop = frame[y1:y2, x1:x2]
 
+    # makes color detection easier -> detect in Hue Space which is more stable under lighting
     hsv = cv.cvtColor(crop, cv.COLOR_BGR2HSV)
     mask = cv.inRange(hsv, np.array(LOW_ORANGE), np.array(HIGH_ORANGE))
 
@@ -58,11 +59,13 @@ def detect_ball_center_stub(frame, roi=None):
         if area < 200 or area > 20000:
             continue
 
+        # ensure that the object is basketball shaped
         (x, y), radius = cv.minEnclosingCircle(c)
         if radius < 5 or radius > 80:
             continue
 
         # Simple score -> bigger and more compact circles
+        # Try to reject reflections, clothes, and other object that aren't the ball
         score = area / (radius * radius + 1e-6)
         if score > best_score:
             best_score = score
@@ -73,11 +76,9 @@ def detect_ball_center_stub(frame, roi=None):
 
     (x, y), radius = cv.minEnclosingCircle(best)
 
+    # Convert the coordinates back to full frame
     cx = int(x) + x1
     cy = int(y) + y1
-
-    if cy < 300:
-        return None, None, mask
 
     return (cx, cy), int(radius), mask
 
@@ -89,6 +90,9 @@ def main():
         raise RuntimeError(f"Could not open video: {VIDEO_PATH}")
 
     centers = []
+
+    # track misses
+    misses = 0
 
     # used for toggling mask on and off
     show_mask = False
@@ -105,19 +109,20 @@ def main():
         if center is not None:
             cx, cy = center
 
-            if cy < 300:
-                center = None
-            else:
+            if cy >= 300:
                 if len(centers) == 0:
                     centers.append(center)
                 else:
                     px, py = centers[-1]
                     if abs(cx - px) < 250 and abs(cy - py) < 250:
+                        # draw the trail
                         centers.append(center)
-            # save the center to the list
+
             centers = centers[-200:]
 
-            if center is not None:
+            if center is not None and radius is not None:
+                print("detected", center, "r=", radius, "misses=", misses, "centers=", len(centers))
+                cx, cy = center
                 cv.circle(frame, (cx, cy), max(radius, 6), (0, 255, 0), 2)
                 cv.circle(frame, (cx, cy), 3, (0, 255, 0), -1)
 
